3b455666fe16c26ea1284330d12a0d2ff8696a19
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-01-20 17:22:24 +0800

    Create and init node class with comparable module

---
+++ lib/node.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module BinarySearchTree
+  # A node on the tree
+  class Node
+    include comparable
+
+    def initialize
+      @data = nil
+      @left_child = nil
+      @right_child = nil
+    end
+
+    def precedence
+      @data
+    end
+
+    def <=>(other)
+      precedence <=> other.precedence
+    end
+  end
+end
